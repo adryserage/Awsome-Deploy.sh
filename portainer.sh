dce67372db2323a956518646f50ed57930fa137c
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-12-10 17:58:18 -0500

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -6,4 +6,4 @@ docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    cr.portainer.io/portainer/portainer-ce:2.9.3
+    cr.portainer.io/portainer/portainer-ce:latest
