c091965349c064c6b8456bb5f502714ce945debf
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-12-10 17:02:59 -0500

    Create portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,8 @@
+docker volume create portainer_data
+docker run -d -p 9443:9443 -p 8000:8000 \
+    --name portainer --restart always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer-data:/data \
+    -v /etc/letsencrypt/live/app.allowebs.com:/certs/live/yourdomain:ro \
+    -v /etc/letsencrypt/archive/app.allowebs.com:/certs/archive/yourdomain:ro \
+    cr.portainer.io/portainer/portainer-ce:2.9.3 --sslcert /certs/live/app.allowebs.com/cert.pem --sslkey /certs/live/app.allowebs.com/privkey.pem
