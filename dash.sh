8feb022ab1bfc98de1830bb4a0c0abbe8e0feb2a
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-12-10 22:00:18 -0500

    Update dash.sh

--- dash.sh
+++ dash.sh
@@ -1,1 +1,6 @@
-docker run -p 80:80 lissy93/dashy
+docker run -d \
+  -p 80:80 \
+  -v /root/my-local-conf.yml:/app/public/conf.yml \
+  --name dashboard \
+  --restart=always \
+  lissy93/dashy:latest
