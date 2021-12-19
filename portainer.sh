6e92c917d77ff892d924c8478423e85df14e39a4
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-12-19 07:45:17 -0500

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -1,7 +1,9 @@
-docker run -d -p 9443:9443 \
-    --name portainer --restart always \
+#!/bin/bash
+
+docker volume create portainer_data
+
+docker run -d -p 9443:9443 --name portainer \
+    --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer-data:/data \
-    -v /etc/letsencrypt/live/app.allowebs.com:/certs/live/app.allowebs.com:ro \
-    -v /etc/letsencrypt/archive/app.allowebs.com:/certs/archive/app.allowebs.com:ro \
-    cr.portainer.io/portainer/portainer-ce:latest --sslcert /certs/live/app.allowebs.com/cert.pem --sslkey /certs/live/app.allowebs.com/privkey.pem
+    -v portainer_data:/data \
+    cr.portainer.io/portainer/portainer-ce:latest
