f09d5a51ec15e9f3f93b38e19e7b81b7f44f3543
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-12-19 00:12:05 -0500

    Update ajenti.sh

--- ajenti.sh
+++ ajenti.sh
@@ -1,1 +1,6 @@
+echo "deb http://security.ubuntu.com/ubuntu xenial-security main universe" | sudo tee -a /etc/apt/sources.listapt upgrade
+
+bash update
+
 wget -O- https://raw.github.com/ajenti/ajenti/1.x/scripts/install-ubuntu.sh | sudo sh
+
