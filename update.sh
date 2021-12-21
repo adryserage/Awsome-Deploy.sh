0c929937463cfbb24ec5cbbd6aab135702c7f15f
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-12-20 20:04:08 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,7 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-
 sudo curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
-
 sudo apt autoremove -y
 sudo apt-get clean -y
 sudo apt-get autoremove --purge -y
