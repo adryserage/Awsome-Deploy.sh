be7e482e8b6586dfa022716ead1c89a3f5deaadf
KazeroG <17680194+KazeroG@users.noreply.github.com>
2022-01-03 16:03:13 -0500

    Update autoheal.sh

--- autoheal.sh
+++ autoheal.sh
@@ -3,4 +3,4 @@ docker run -d \
     --restart=always \
     -e AUTOHEAL_CONTAINER_LABEL=all \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    willfarrell/autoheal
+    willfarrell/autoheal:latest
