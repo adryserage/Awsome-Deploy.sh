0de21acc7b2a63246c00089db43c0ed3cc8f5f69
KazeroG <17680194+KazeroG@users.noreply.github.com>
2022-01-04 00:44:31 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ sudo curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
 sudo apt autoremove -y
 sudo apt-get clean -y
 sudo apt-get autoremove --purge -y
-
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 dpkg-reconfigure tzdata
 
 
